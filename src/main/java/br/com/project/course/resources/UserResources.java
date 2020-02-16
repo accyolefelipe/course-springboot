30dd72309f945266bd3820ad5ef68477649d3b7a
Edvan Felipe Accyole <accyole@hotmail.com>
2020-02-16 18:14:37 -0300

    User Insert , Status 201 (created)

--- src/main/java/br/com/project/course/resources/UserResources.java
+++ src/main/java/br/com/project/course/resources/UserResources.java
@@ -1,13 +1,17 @@
 package br.com.project.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.project.course.entities.User;
 import br.com.project.course.services.UserService;
@@ -29,5 +33,12 @@ public class UserResources {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
--- src/main/java/br/com/project/course/services/UserService.java
+++ src/main/java/br/com/project/course/services/UserService.java
@@ -1,6 +1,5 @@
 package br.com.project.course.services;
 
-
 import java.util.List;
 import java.util.Optional;
 
@@ -10,19 +9,24 @@ import org.springframework.stereotype.Service;
 import br.com.project.course.entities.User;
 import br.com.project.course.repositories.UserRepository;
 
-@Service 
+@Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
+	}
+
+	public User insert(User obj) {
+		return userRepository.save(obj);
+
 	}
 
 }
