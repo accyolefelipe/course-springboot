77036c0c69d06e597fe2ab67f98205623932f599
Edvan Felipe Accyole <accyole@hotmail.com>
2020-02-16 17:16:53 -0300

    Entidade Payment, OneToOne

--- src/main/java/br/com/project/course/config/TestConfig.java
+++ src/main/java/br/com/project/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.project.course.entities.Category;
 import br.com.project.course.entities.Order;
 import br.com.project.course.entities.OrderItem;
+import br.com.project.course.entities.Payment;
 import br.com.project.course.entities.Product;
 import br.com.project.course.entities.User;
 import br.com.project.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 		
 		
