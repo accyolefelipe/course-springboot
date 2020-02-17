920f4b88ac513a66b9b541bd027749461cc7be7b
Edvan Felipe Accyole <accyole@hotmail.com>
2020-02-17 00:01:52 -0300

    Tratamento de erro no update

--- src/main/java/br/com/project/course/services/UserService.java
+++ src/main/java/br/com/project/course/services/UserService.java
@@ -3,6 +3,8 @@ package br.com.project.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -32,28 +34,32 @@ public class UserService {
 		return userRepository.save(obj);
 
 	}
-	
+
 	public void delete(Long id) {
 		try {
-		userRepository.deleteById(id);
+			userRepository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(e);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = userRepository.getOne(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getOne(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 
 }
