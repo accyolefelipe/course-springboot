b371d4cf754625c4fb6d239b8af4c73d2f945be6
Edvan Felipe Accyole <accyole@hotmail.com>
2020-02-16 23:28:42 -0300

    Tratamento de erro, Resource not found

--- src/main/java/br/com/project/course/services/UserService.java
+++ src/main/java/br/com/project/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.project.course.entities.User;
 import br.com.project.course.repositories.UserRepository;
+import br.com.project.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
